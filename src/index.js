0aed762a901239311da6c6d27f289fa87e655d1f
Krish <your-github-email@gmail.com>
2026-08-21 15:27:10 +0530

    utilcode

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 
-// require('dotenv').config({path: './env'})
-
-import dotenv from "dotenv"
-
-// import mongoose from "mongoose"
-// import { DB_NAME } from "./constants";
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './.env'
 
 });
 
-connectDB();
+connectDB()
+.then(()=>{
+
+      app.on("error",(error)=>{
+        console.log("ERRR: ", error);
+        throw error
+        });
+        
+        app.listen(process.env.PORT || 8000, ()=>{
+            console.log(`Server is runing at port : ${process.env.PORT}`)
+        })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!!!!!! ", err);
+})
 
 
 
